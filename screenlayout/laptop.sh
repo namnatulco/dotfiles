2a1f656b8e66ebaee8715be4b8ce8413d4cb5ffa
Rens van der Heijden <rens.vanderheijden@uni-ulm.de>
2015-08-13 08:53:15 +0200

    screen layout config for laptop only

---
+++ screenlayout/laptop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#note; for some reason this cannot be done in one command (for xrandr 1.4.3/1.4)
+#disable all but large monitor
+xrandr --output VIRTUAL1 --off --output DP3 --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --off
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output VGA1 --off
