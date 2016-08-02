96aec1e0f096371a4b1c0e7c9fa3558e820354bd
Rens van der Heijden <rens.vanderheijden@uni-ulm.de>
2016-08-02 15:08:48 +0200

    remove superfluous option

--- screenlayout/workspace.sh
+++ screenlayout/workspace.sh
@@ -4,5 +4,3 @@
 xrandr --output VIRTUAL1 --off --output DP3 --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --off
 #enable second monitor
 xrandr --output HDMI2 --mode 1920x1200 --pos 2560x0 --rotate left
-#add nodeadkeys option to keyboard settings
-setxkbmap -layout us -variant altgr-intl -option nodeadkeys
